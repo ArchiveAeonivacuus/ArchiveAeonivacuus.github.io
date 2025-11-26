4683320286034a7594f2a70d76c92ece1b437949
SakiMidare <ssmzhn@163.com>
2025-11-26 09:22:44 +0800

    modified the site link

--- astro.config.mjs
+++ astro.config.mjs
@@ -29,7 +29,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://ArchiveAeonivacuus.github.io/",
+	site: "https://www.瀛寰.cn/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
