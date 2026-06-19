2fcec3814269e020d9e34e2b7ef7029c6ba92354
SakiMidare <ssmzhn@163.com>
2026-06-19 18:14:33 +0800

    Removed `astro-critter` dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,6 @@ import swup from "@swup/astro";
 import { defineConfig } from "astro/config";
 import expressiveCode from "astro-expressive-code";
 import icon from "astro-icon";
-import critters from "astro-critters";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeComponents from "rehype-components";/* Render the custom directive content */
 import rehypeKatex from "rehype-katex";
@@ -34,7 +33,7 @@ import mermaid from "astro-mermaid";
 export default defineConfig({
     site: "https://www.瀛寰.cn/",
     trailingSlash: "always",
-    integrations: [critters(), tailwind({
+    integrations: [tailwind({
         nesting: true,
 		}), swup({
         theme: false,
